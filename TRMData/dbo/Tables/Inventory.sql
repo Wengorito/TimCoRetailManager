a13be5b7cc1609db1cfa13a81bda3ed3eb8c8499
Grzegorz <hvitr.ulfr@gmail.com>
2023-08-13 17:10:23 +0200

    Add Foreign Key Relationships
    Adds FKs to related tables.

--- TRMData/dbo/Tables/Inventory.sql
+++ TRMData/dbo/Tables/Inventory.sql
@@ -3,7 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [ProductId] INT NOT NULL, 
     [Quantity] INT NOT NULL DEFAULT 1, 
-    [PurchasePrice] MONEY NOT NULL, 
-    [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
-
+    [PurchasePrice] MONEY NOT NULL, CONSTRAINT [FK_Inventory_ToProduct] FOREIGN KEY (ProductId) REFERENCES [Product]([Id]),
+    [PurchaseDate] DATETIME2 NOT NULL DEFAULT getutcdate()
 )
--- TRMData/dbo/Tables/SaleDetail.sql
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[SaleDetail]
 (
-	[Id] INT NOT NULL PRIMARY KEY,
-	[SaleId] INT NOT NULL,
-	[ProductId]  INT NOT NULL,
-	[Quantity] INT NOT NULL DEFAULT 1,
-	[PurchasePrice] MONEY NOT NULL,
-	[Tax] MONEY NOT NULL DEFAULT 0
+    [Id] INT NOT NULL PRIMARY KEY,
+    [SaleId] INT NOT NULL,
+    [ProductId]  INT NOT NULL,
+    [Quantity] INT NOT NULL DEFAULT 1,
+    [PurchasePrice] MONEY NOT NULL,
+    [Tax] MONEY NOT NULL DEFAULT 0, 
+    CONSTRAINT [FK_SaleDetail_ToSale] FOREIGN KEY (SaleId) REFERENCES Sale(Id),
+    CONSTRAINT [FK_SaleDetail_ToProduct] FOREIGN KEY (SaleId) REFERENCES Product(Id)
 )
